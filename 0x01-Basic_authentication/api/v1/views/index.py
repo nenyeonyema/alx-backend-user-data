83d9049def7163f242f455917dc634207f9733ed
nenyeonyema <chinenyeonyema1@gmail.com>
2024-06-08 06:23:22 -0700

    unauthorized error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,7 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_viewa.route('/unauthorized', methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unautorized() -> str:
     """ GET /api/v1/unauthorized
     Raise:
