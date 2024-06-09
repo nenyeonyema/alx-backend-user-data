44174fb2183a5b1c4e42297bcd0e6a1a84110648
nenyeonyema <chinenyeonyema1@gmail.com>
2024-06-09 06:29:11 -0700

    required auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,20 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Require authentication
         """
-        return False
+        if path is None:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        path_slashed = path if path.endswith('/') else path + '/'
+
+        for each_path in excluded_paths:
+            slashed_excluded_paths = each_path if each_path.endswith('/') else each_path + '/'
+            if path_slashed == slashed_excluded_paths:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Authorizes header
