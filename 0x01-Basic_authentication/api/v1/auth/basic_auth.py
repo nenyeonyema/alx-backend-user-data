f53265273dddfeda5f192201ea44ea5eec0f1952
nenyeonyema <chinenyeonyema1@gmail.com>
2024-06-10 11:33:38 -0700

    base64 headers

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -97,3 +97,29 @@ class BasicAuth(Auth):
             return None
 
         return user
+
+    def current_user(self, request=None) -> Optional[TypeVar('User')]:
+        """
+        Retrieves the User instance for a request.
+
+        :param request: The Flask request object.
+        :return: The User instance if credentials are valid, otherwise None.
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+
+        base64_auth_header = self.extract_base64_authorization_header(auth_header)
+        if base64_auth_header is None:
+            return None
+
+        decoded_auth_header = self.decode_base64_authorization_header(base64_auth_header)
+        if decoded_auth_header is None:
+            return None
+
+        user_email, user_pwd = self.extract_user_credentials(decoded_auth_header)
+        if user_email is None or user_pwd is None:
+            return None
+
+        user = self.user_object_from_credentials(user_email, user_pwd)
+        return user
