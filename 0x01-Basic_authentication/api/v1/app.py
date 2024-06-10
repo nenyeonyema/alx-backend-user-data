9cf564e38d8e4503c372d5801556b86d8a9fb8a4
nenyeonyema <chinenyeonyema1@gmail.com>
2024-06-10 11:09:17 -0700

    modified

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -51,8 +51,11 @@ def before_request_func():
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', 
-            '/api/v1/forbidden/']
+    excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/'
+            ]
     if not auth.require_auth(request.path, excluded_paths):
         return
 
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,18 +5,21 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """ 
+    """
     BasicAuth class that inherits from Auth.
     Implements Basic Authentication methods.
     """
 
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """ 
+        """
         Extracts the Base64 part of the Authorization header for Basic Authentication.
         Returns None if the header is invalid or does not start with 'Basic '.
 
-        param: - authorization_header: The Authorization header string.
-        return: - The Base64 encoded part of the header, or None if invalid.
+        Parameters:
+        authorization_header (str): The Authorization header string.
+
+        Returns:
+        (str): The Base64 encoded part of the header, or None if invalid.
         """
         if authorization_header is None:
             return None
