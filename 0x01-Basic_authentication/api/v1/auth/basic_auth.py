d46cb7dc9a4ff055eb8dceb4d9bb634723492454
nenyeonyema <chinenyeonyema1@gmail.com>
2024-06-10 09:46:01 -0700

    basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+""" Basic Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class that inherits from Auth.
+    Currently, it does not add any new functionality.
+    """
+    pass
