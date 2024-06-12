db50a0af9f20fb2215aa444418cfdbb09f14fd1e
nenyeonyema <chinenyeonyema1@gmail.com>
2024-06-12 08:57:32 -0700

    session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -5,19 +5,23 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
+from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
 app = Flask(__name__)
-CORS(app)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 auth_type = os.getenv('AUTH_TYPE', 'auth')
 
 if auth_type == 'basic_auth':
     auth = BasicAuth()
+elif auth_type == 'session_auth':
+    auth = SessionAuth()
 else:
     auth = Auth()
 
