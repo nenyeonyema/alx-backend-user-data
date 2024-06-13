d19bcfdc0e92e905cc3d8968bd9efa4b0fbb7127
nenyeonyema <chinenyeonyema1@gmail.com>
2024-06-13 08:16:37 -0700

    user auth

--- 0x03-user_authentication_service/user1.py
+++ 0x03-user_authentication_service/user1.py
@@ -13,7 +13,16 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(250), unique=True, nullable=False)
+    email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        return "<User(id='%d', email='%s', hashed_password='%s', session_id='%s', reset_token='%s')>" % (
+                                self.id,
+                                self.email,
+                                self.hashed_password,
+                                self.session_id,
+                                self.reset_token
+                                )
