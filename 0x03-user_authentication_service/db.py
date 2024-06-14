c228d3bc9e26e155b1be2432d13b5e109e354910
nenyeonyema <chinenyeonyema1@gmail.com>
2024-06-14 03:35:06 -0700

    add's user to db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_(self, email: str, hashed_password: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ Adds new user
 
             Parmeters:
